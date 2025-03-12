7383a8b6298a341ab0c74b8c67588bcf178ace34
Manokha Vadim <manokha.vadim@gmail.com>
2025-03-12 11:57:10 +0200

    add new file task-4.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,12 @@
-function makeTransaction(quantity, pricePerDroid) {
-  // Обчислюємо загальну вартість замовлення
-  const totalPrice = quantity * pricePerDroid;
+// function makeTransaction(quantity, pricePerDroid) {
+//   // Обчислюємо загальну вартість замовлення
+//   const totalPrice = quantity * pricePerDroid;
 
-  // Формуємо і повертаємо рядок з повідомленням
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
+//   // Формуємо і повертаємо рядок з повідомленням
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
 
-// Перевірка функції
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// // Перевірка функції
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
