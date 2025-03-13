d9561a0a85b151a0b32b4bddb11c9ec3ec88347e
Manokha Vadim <manokha.vadim@gmail.com>
2025-03-13 19:47:15 +0200

    modified task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -76,3 +76,17 @@
 // console.log(createReversedArray(412, 371, 94, 63, 176)); // [176, 63, 94, 371, 412]
 // console.log(createReversedArray()); // []
 // console.log(createReversedArray(1)); // [1]
+
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+
+  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+}
+
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
